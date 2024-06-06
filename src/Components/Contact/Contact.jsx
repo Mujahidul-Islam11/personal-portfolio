0aff5d90e3abb7381e2cd67b2e7398e9cfb74137
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-07 00:49:43 +0600

    Contact page description responsive style changed

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <h3 className="text-3xl md:text-5xl font-bold text-center">
           Contact with Me
         </h3>
-        <p className="text-center text-gray-600 text-lg">
+        <p className="md:text-lg text-center text-gray-600">
           Reach out and let's chat! I'm here to answer any questions you may
           have.
         </p>
